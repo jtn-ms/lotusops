18ec00a83299bc5a32ffce766151dd80741bf10d
frank <allset4go@gmail.com>
2021-10-29 14:44:21 -0400

    refine

--- lotusops/speedtest/textformat.py
+++ lotusops/speedtest/textformat.py
@@ -4,5 +4,5 @@ def analyzeFile(filepath,filters):
         lines=f.readlines()
         for line in lines:
             if any(filter not in line for filter in filters): continue
-            structed=line.strip("\t")
-            print(line)
+            # structed=line.strip("\t")
+            print(line.rstrip("\n"))
