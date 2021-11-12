cd29e4d55ee0ae7e090f2eff90332c703cad66ec
frank <allset4go@gmail.com>
2021-11-12 03:41:11 -0500

    format changed

--- lotusops/speedtest/textformat.py
+++ lotusops/speedtest/textformat.py
@@ -10,7 +10,7 @@ from lotusops.speedtest.jsonformat import string2datetime,msg2sectorId
 from lotusops.speedtest.sectorstatus import strfdelta
 
 def analyzeFile(filepaths,filters):
-    total_cnt=0
+    total_cnt,total_sum=0,datetime.timedelta(0)
     start,finish=string2datetime("2089-01-01T00:00:00.000"),string2datetime("1989-01-01T00:00:00.000")
     for filepath in filepaths:
         sector,sectors={},{}
@@ -57,15 +57,29 @@ def analyzeFile(filepaths,filters):
                 sum+=v["period"]
             mean_=sum/cnt if cnt!=0 else sum
             total_cnt+=cnt
+            total_sum+=sum
             print("*******************************************")  
             print("MIN:  {0}".format(strfdelta(min_)))
             print("MAX:  {0}".format(strfdelta(max_)))
             print("MEAN: {0}".format(strfdelta(mean_)))
             print("*******************************************")
     # speed calculation
+    if total_cnt == 0: return
     duration=(finish-start).total_seconds()/(24*3600.0)#days
+    meantime=(total_sum/total_cnt).total_seconds()/(24*3600.0)#days
     sector_size = 32#GiB
     _as_terabyte = sector_size/1000.0
-    speed = total_cnt*_as_terabyte/duration
-    print("SPEED(32GiB*{0}/{1}): {2}TiB/Day, START: {3}, FINISH: {4}".format(total_cnt,"%.1f days"%duration,"%.2f"%speed,start,finish,))
+    dspeed = total_cnt*_as_terabyte/duration
+    mspeed = total_cnt*_as_terabyte/meantime
+    print("SectorSize: 32GiB, SectorCnt: {0}, MeanTime: {1}".\
+        format(total_cnt,\
+              "%.1f mins"%(meantime*24)))
+    print("DURATION:{0} START: {1}, FINISH: {2}".\
+        format(strfdelta(finish-start),\
+            start.strftime("%Y-%m-%d %H:%M:%S"),\
+            finish.strftime("%Y-%m-%d %H:%M:%S")))
+    if any("seal_pre_commit_phase1" in filter for filter in filters):
+        print("MINING RATE: {0}".format("%.1fT/D"%dspeed))
+    else:
+        print("MINING RATE: {0}|{1}".format("%.1fT/D"%dspeed,"%.1fT/D"%mspeed))
 
